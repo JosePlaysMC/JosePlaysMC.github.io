3787b5f183c33387aba222e2419edcb92e3005fa
JosePlaysMC <33184807+JosePlaysMC@users.noreply.github.com>
2018-03-19 12:10:08 +0000

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="/styles/style.css">
+    <meta charset="utf-8">
+    <title>I Need a Website</title>
+  </head>
+  <body>
+    <script>
+    function Home() {
+      window.location.href = "index.php";
+    }
+    function Submissions() {
+      window.location.href = "submissions.php";
+    }
+    function About() {
+      window.location.href = "about.php";
+    }
+    </script>
+    <div class="div-top">
+    <table>
+      <tr>
+        <td style="background-color: #6391dd;" onClick="Home();">Home</td>
+        <td onClick="Submissions();">Submissions</td>
+        <td onClick="About();">About</td>
+      </tr>
+    </table>
+  </div>
+  <div class="p1">
+  <p id="indextext">Welcome to INeedAWebsite! Here you can submit a form to get a cheap website with prices ranging from $5-$15! When making websites, we make sure we use the best practices.
+  Websites can contain: CSS, PHP, HTML, and Javascript. We guarantee your website in 1 week or you get your money back! You can choose us over other website designers! Keep in
+mind we cannot use paid services for your website unless you (the user) already bought and sent us the service! We are fluent in english and spanish and we know PHP, SQL, HTML, CSS, C++, C#,
+Bash, Batch, and Java. No other website designers can match or beat our prices!</p>
+  </div>
+  <div class="p1">
+  <img class="cursor" src="http://discover.superco.net/wp-content/uploads/2014/03/http-www-cursor-web-address-blocking-access-internet-370x229.jpg"></img>
+  </div>
+  </body>
+</html>
